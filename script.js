ef162b4ad90a68fa60d308f78744e1dce7d960ee
Brian <shidoitsukaa@gmail.com>
2025-09-10 12:42:44 +0800

    w

--- script.js
+++ script.js
@@ -3,7 +3,12 @@ const isiButton = document.createTextNode("CLICK");
 tagButton.appendChild(isiButton);
 
 const tagImage = document.createElement("img");
-tagImage.setAttribute("src", "/asset/vibingCat.gif");
+const logoImage = document.createElement("img");
+const text = document.createElement("h1");
+tagImage.setAttribute("src", "./asset/vibingCat.gif");
+logoImage.setAttribute("src", "./asset/logo.jpg");
+text.innerHTML = 'you just won $500'
+text.style.position = 'absolute'
 tagButton.style.padding = "15px 32px";
 
 document.body.appendChild(tagButton);
@@ -12,7 +17,11 @@ const audio = new Audio("./asset/Engelwood.mp3");
 
 tagButton.onclick = () => {
   audio.play();
-  document.body.appendChild(tagImage);
+  setTimeout(() => {
+    document.body.appendChild(tagImage);
+    document.body.appendChild(logoImage);
+    document.body.appendChild(text);
+  }, 1500);
   document.body.removeChild(tagButton);
   setInterval(() => {
     document.body.style.backgroundColor =
